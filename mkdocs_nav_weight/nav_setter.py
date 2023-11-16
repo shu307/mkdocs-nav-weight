4b0d8957c74ec345729c1c7230ada7e2e4146fff
shu <26036149+shu307@users.noreply.github.com>
2023-11-16 23:54:25 +0800

    Update nav_setter.py

--- mkdocs_nav_weight/nav_setter.py
+++ mkdocs_nav_weight/nav_setter.py
@@ -21,30 +21,30 @@ class NavSetter():
     def __init__(self, nav: Navigation, config, mkdocs_config) -> None:
         # options
         self.is_section_renamed = config[SECTION_RENAMED]
-        self._index_weight = config[INDEX_WEIGHT]
-        self._is_warning = config[WARNING]
-        self._is_reverse = config[REVERSE]
-        self._is_headless_included = config[HEADLESS_INCLUDED]
+        self.index_weight = config[INDEX_WEIGHT]
+        self.is_warning = config[WARNING]
+        self.is_reverse = config[REVERSE]
+        self.is_headless_included = config[HEADLESS_INCLUDED]
 
         # mkdocs
         self.nav = nav
         self.mkdocs_config = mkdocs_config
 
         # utils
-        self._meta_defaults = dict(
-            WEIGHT=0,
-            RETITLED=False,
-            EMPTY=False,
-            HEADLESS=False
-        )
+        self._meta_defaults = {
+            WEIGHT: 0,
+            HEADLESS: False,
+            RETITLED: False,
+            EMPTY: False
+        }
         """fallback for unset or invalid value"""
 
-        self._meta_types = dict(
-            WEIGHT=Number,
-            RETITLED=bool,
-            EMPTY=bool,
-            HEADLESS=bool
-        )
+        self._meta_types = {
+            WEIGHT: Number,
+            HEADLESS: bool,
+            RETITLED: bool,
+            EMPTY: bool
+        }
         """valid types for values"""
 
         self._section_weights = {}
@@ -64,7 +64,7 @@ class NavSetter():
             if isinstance(meta[key], self._meta_types[key]):
                 result = meta[key]
             # option: "warning"
-            elif self._is_warning:
+            elif self.is_warning:
                 log.warning(
                     f"[mkdocs-nav-weight]: Invaild value for \"{key}\" in {page}, setting to \"{result}\"")
 
@@ -74,19 +74,19 @@ class NavSetter():
         result = 0
 
         if item.is_page:
-            result = self._index_weight if item.is_index else self._get_meta(
+            result = self.index_weight if item.is_index else self._get_meta(
                 WEIGHT,  item)
-        if item.is_section:
+        elif item.is_section:
             result = self._section_weights[item]
 
         return result
 
-    def _sort_items(self, items: list) -> Number:
+    def _sort_nav_items(self, items: list) -> Number:
         parent_weight = 0
 
         for item in items:
             if item.is_section:
-                weight = self._sort_items(item.children)
+                weight = self._sort_nav_items(item.children)
                 self._section_weights[item] = weight
             elif item.is_page:
                 item.read_source(self.mkdocs_config)
@@ -94,18 +94,19 @@ class NavSetter():
                 if not item.is_index:
                     if self._get_meta(HEADLESS,  item):
                         self._headless_items.append(item)
-                # index, but not a top level "index" -- homepage
+                # index, not homepage
                 elif item.parent:
+                    # set parent
                     parent_weight = self._get_meta(WEIGHT, item)
-                    # option: "section_renamed"
+                    if self._get_meta(HEADLESS, item):
+                        self._headless_items.append(item.parent)
+                    # set self
                     if self.is_section_renamed or self._get_meta(RETITLED, item):
                         item.parent.title = item.title
                     if self._get_meta(EMPTY,  item):
                         self._empty_items.append(item)
-                    if self._get_meta(HEADLESS_INCLUDED, item):
-                        self._headless_items.append(item.parent)
 
-        items.sort(key=self._get_weight, reverse=self._is_reverse)
+        items.sort(key=self._get_weight, reverse=self.is_reverse)
 
         return parent_weight
 
@@ -116,28 +117,28 @@ class NavSetter():
             if item.is_section:
                 return self._get_first_page(item.children)
 
-    def _do_connection_rec(self, items: list, temp_page: Page) -> Page:
+    def _do_connection(self, items: list, temp_page: Page) -> Page:
         for item in items:
             if item.is_page:
                 item.previous_page = temp_page
                 temp_page.next_page = item
                 temp_page = item
             elif item.is_section:
-                temp_page = self._do_connection_rec(
+                temp_page = self._do_connection(
                     item.children, temp_page)
 
         return temp_page
 
-    def _do_connection(self, items: list) -> None:
+    def _pre_connection(self, items: list) -> None:
         first_page = self._get_first_page(items)
 
         if first_page:
-            last_page = self._do_connection_rec(items, first_page)
+            last_page = self._do_connection(items, first_page)
             first_page.previous_page = None
             last_page.next_page = None
 
     def _do_headless(self, item: Page | Section) -> None:
-        # page
+        # headless page
         if item.is_page:
             if item.previous_page:
                 item.previous_page.next_page = item.next_page
@@ -145,17 +146,17 @@ class NavSetter():
             if item.next_page:
                 item.next_page.previous_page = item.previous_page
                 item.next_page = None
-        # section
+        # headless section
         elif item.is_section:
-            self._do_connection(item.children)
+            self._pre_connection(item.children)
 
-        # is top level?
+        # remove item (should set item.parent to None?)
         if item.parent:
             item.parent.children.remove(item)
         else:
             self.nav.items.remove(item)
 
-    def _handle_items(self) -> None:
+    def _handle_nav_items(self) -> None:
         # isolate items
         for item in self._empty_items:
             self._do_headless(item)
@@ -163,7 +164,7 @@ class NavSetter():
             self._do_headless(item)
 
         # connect normal items
-        self._do_connection(self.nav.items)
+        self._pre_connection(self.nav.items)
 
     def _get_nav_pages(self, items: list) -> list:
         pages = []
@@ -178,12 +179,12 @@ class NavSetter():
 
     def set_nav(self) -> Navigation:
         # nav_items
-        self._sort_items(self.nav.items)
-        self._handle_items()
+        self._sort_nav_items(self.nav.items)
+        self._handle_nav_items()
 
         # nav_pages
         self.nav.pages = self._get_nav_pages(self.nav.items)
-        if self._is_headless_included:
+        if self.is_headless_included:
             self.nav.pages.extend(self._get_nav_pages(self._headless_items))
 
         return self.nav
